8fc22f9a33e90086f28d8beb20440242b0674fa0
Rafał Mikołajun <root@rmweb.pl>
2023-11-03 15:21:43 +0100

    add image methods to RSS Item

--- app/src/main/java/app/rssreader/domain/dto/rss/Item.java
+++ app/src/main/java/app/rssreader/domain/dto/rss/Item.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import app.rssreader.application.logic.html.HtmlUtil;
 import lombok.Data;
 
 @Data
@@ -21,4 +25,29 @@ public class Item {
     @JacksonXmlProperty(localName = "enclosure")
     @JacksonXmlElementWrapper(useWrapping = false)
     private List<Enclosure> enclosures;
+
+    public String getImageUrl() {
+        String url = null;
+        List<Enclosure> images = getImagesEnclosures();
+
+        if (!images.isEmpty()) {
+            url = images.get(0).getUrl();
+        }
+
+        if (url == null) {
+            List<String> urls = HtmlUtil.findImagesSrc(description);
+
+            if (!urls.isEmpty()) {
+                url = urls.get(0);
+            }
+        }
+
+        return url;
+    }
+
+    public List<Enclosure> getImagesEnclosures() {
+        return Optional.ofNullable(enclosures).orElse(new ArrayList<>()).stream().filter(
+            enclosure -> enclosure.getType().startsWith("image/")
+        ).collect(Collectors.toList());
+    }
 }
