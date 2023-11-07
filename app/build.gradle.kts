b6fccb8f8dc6583ec5d8aabe61fb19b6eb720454
Rafał Mikołajun <root@rmweb.pl>
2023-11-07 19:50:45 +0100

    app theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // App dependencies
+    // https://mvnrepository.com/artifact/com.google.android.material/material
+    runtimeOnly("com.google.android.material:material:1.10.0")
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
--- app/src/main/java/app/rssreader/MainActivity.kt
+++ app/src/main/java/app/rssreader/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.rssreader.application.MainService
-import app.rssreader.ui.theme.RSSReaderTheme
+import app.rssreader.ui.theme.AppTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +21,14 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
         setContent {
-            RSSReaderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                    main.main()
-                }
-            }
+            AppLayout()
+            main.main()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Hello(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -46,8 +37,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RSSReaderTheme {
-        Greeting("Android")
+fun AppLayout() {
+    AppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Hello("Android")
+        }
     }
 }
