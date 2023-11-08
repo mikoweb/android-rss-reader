e13e28b9d18b2db35eaa7902832a92d4d7ef75b3
Rafał Mikołajun <root@rmweb.pl>
2023-11-08 01:23:08 +0100

    update MainActivity

--- app/src/main/java/app/rssreader/MainActivity.kt
+++ app/src/main/java/app/rssreader/MainActivity.kt
@@ -1,48 +1,34 @@
 package app.rssreader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import app.rssreader.application.MainService
 import app.rssreader.ui.theme.AppTheme
+import app.rssreader.ui.theme.element.RssTextBoxViewModel
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
-    @Inject lateinit var main: MainService
+    @Inject
+    lateinit var rssTextBoxViewModel: RssTextBoxViewModel;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as MainApplication).appComponent.inject(this)
 
         super.onCreate(savedInstanceState)
+
+        Log.i("Sample", "LOAD ACTIVITY")
+
         setContent {
             AppLayout()
-            main.main()
         }
     }
-}
-
-@Composable
-fun Hello(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun AppLayout() {
-    AppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            Hello("Android")
-        }
-    }
+    AppTheme()
 }
